0bad42a5e08adc9dd4bb993f48e17f0300e27a3a
arinda jo <arindajosey7@gmail.com>
2024-10-28 20:52:28 +0300

    perfect

---
+++ srcipt.js
@@ -0,0 +1,12 @@
+let health = 100;
+let Gold = 50;
+let CurrrentWeapon = 0;
+let fighting;
+let MonsterHealth;
+let Inventory =  ["stick"];
+const button1 = document.querySelector("#button1");
+//intialize button
+button1.onClick = Seeall;
+function seeall(){
+    console.log("viewing all the images available");
+}
